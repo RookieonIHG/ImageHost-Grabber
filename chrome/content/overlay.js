2c7af4ccb9a8c94c48b7231a51a47be60ba0ab26
Rookie <Thierry@.(none)>
2011-06-05 18:23:14 +0200

    In the 'contentAreaContextMenu' the 'Send link to thread sucker' command shows only if the link matches a forum description. The context sensitive menu is more sensitive...
    Signed-off-by: Rookie <Thierry@.(none)>

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,3 +1,15 @@
+function isThread(Url) {
+	var forumStyleFileObj = new ihg_Functions.forumStyleFileService();
+	var fsFile = forumStyleFileObj.getForumStyles();
+	var forums = fsFile.getElementsByTagName("forum");
+	
+	for (var i = 0; i < forums.length; i++) {
+		var uPatNode = forums[i].getElementsByTagName("urlpattern")[0];
+		var uPat = new RegExp(uPatNode.textContent);
+		if (Url.match(uPat)) return true;
+	}
+}
+
 function ihg_contextMenuInit() {
 	var ihg_contextMenu = document.getElementById("contentAreaContextMenu");
 	if (ihg_contextMenu.state == 'open') return;
@@ -7,5 +19,5 @@ function ihg_contextMenuInit() {
 	var isImg = gContextMenu.onImage;
 	var isLnk = gContextMenu.onLink;
 	
-	document.getElementById("suck_sel_thread").hidden = !isLnk;
+	document.getElementById("suck_sel_thread").hidden = !(isLnk && isThread(content.document.activeElement.href));
 }
