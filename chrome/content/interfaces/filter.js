0e7ef72a8deaf9ad961f3eb5ae9f4e45eae9a8b6
Rookie <Thierry@evo-n600.(none)>
2012-12-26 19:27:11 +0100

    New Host ID filter in the Filter Interface Dialog.
    Rigth-click on 'Host ID' column header in the Filter Interface and select hosters.
    
    Signed-off-by: Rookie <Thierry@evo-n600.(none)>

--- chrome/content/interfaces/filter.js
+++ chrome/content/interfaces/filter.js
@@ -23,10 +23,11 @@ function onLoad() {
 	var tree = doc.getElementById("igLinksTree");
 	tree.onselect = changeImage;
 
+	var hostlist = [];
 	var currentId = 0;
 	
 	for (var i = firstPage; i <= lastPage; i++) {
-		for (var j = 0; j < objLinks.links[i].length; j++) {
+		for (var j = 0; j < objLinks.links[i].length; j++, currentId++) {
 			var treeItem = doc.createElement("treeitem");
 			treeItem.setAttribute("id", "req_" + currentId);
 
@@ -46,23 +47,58 @@ function onLoad() {
 			if (objLinks.thumbs[i][j]) treeCell1.setAttribute("src", objLinks.thumbs[i][j].src);
 
 			var treeCell2 = doc.createElement("treecell");
- 			treeCell2.setAttribute("label", objLinks.hostID[i][j]);
- 			treeCell2.setAttribute("value", objLinks.hostID[i][j]);
- 			treeCell2.setAttribute("editable", "false");
- 			treeCell2.setAttribute("id", "host_" + currentId);
+			treeCell2.setAttribute("label", objLinks.hostID[i][j]);
+			treeCell2.setAttribute("value", objLinks.hostID[i][j]);
+			treeCell2.setAttribute("editable", "false");
+			treeCell2.setAttribute("id", "host_" + currentId);
 			
+			if (hostlist.indexOf(objLinks.hostID[i][j]) < 0) hostlist.push(objLinks.hostID[i][j]);
 			
 			treeRow.appendChild(treeCell0);
 			treeRow.appendChild(treeCell1);
 			treeRow.appendChild(treeCell2);
 			treeItem.appendChild(treeRow);
 			list.appendChild(treeItem);
-			currentId++;
 		}
-	}	
+	}
+	
+	if (hostlist.length > 0) {
+		var HostList_popup = doc.getElementById("host_list");
+		hostlist.sort();
+		
+		for (var i = 0; i < hostlist.length; i++) {
+			var menuItem = doc.createElement("menuitem");
+			menuItem.setAttribute("label", hostlist[i]);
+			menuItem.setAttribute("value", hostlist[i]);
+			menuItem.setAttribute("type", "checkbox");
+			menuItem.setAttribute("checked", "true");
+			HostList_popup.appendChild(menuItem);
+			}
+		}
 	
 	if (tree.view.rowCount > 0) tree.view.selection.select(0);
 	chgPreview(true);
+	updateCounter();
+}
+
+
+function MaskHost(HostID_target) {
+	var doc = this.document;
+	var tree = doc.getElementById("igLinksTree");
+	var HostIDidx = tree.columns.getNamedColumn("host").index;
+	
+	if (HostID_target.value == "") {
+		Array.forEach(doc.getElementById("host_list").childNodes, function(menuItem) {menuItem.setAttribute("checked", "true")});
+		Array.forEach(doc.getElementById("list").childNodes, function(req_row) {req_row.removeAttribute("hidden")});
+		HostID_target.removeAttribute("checked");
+		}
+	else
+		Array.filter(doc.getElementById("list").childNodes, function(req_row) {return (req_row.firstChild.childNodes[HostIDidx].getAttribute("label") == HostID_target.value)})
+			.forEach(function(req_row) {
+				if (HostID_target.hasAttribute("checked")) req_row.removeAttribute("hidden");
+				else req_row.hidden = true;
+				});
+	
 	updateCounter();
 }
 
@@ -71,7 +107,7 @@ function doOK() {
 	var doc = this.document;
 	var newObjLinks = new LinksOBJ();
 	var currentId = 0;
-			
+	
 	for (var i = firstPage; i <= lastPage; i++) {
 		newObjLinks.links[i] = new Array();
 		newObjLinks.dirSave[i] = new Array();
@@ -81,7 +117,9 @@ function doOK() {
 		newObjLinks.downloadTimeout[i] = new Array();
 		newObjLinks.originatingPage[i] = new Array();
 		
-		for (var j = 0; j < objLinks.links[i].length; j++) {
+		for (var j = 0; j < objLinks.links[i].length; j++, currentId++) {
+			if (doc.getElementById("req_" + currentId).getAttribute("hidden") == "true") continue;
+			
 			var shakespeare = doc.getElementById("tobeornottobe_" + currentId);
 			
 			if (shakespeare.getAttribute("value") == "true") {
@@ -94,7 +132,6 @@ function doOK() {
 				newObjLinks.originatingPage[i].push(objLinks.originatingPage[i][j]);
 			}
 			
-			currentId++;
 		}
 	}
 
@@ -127,10 +164,16 @@ function doSetAll(value) {
 	var tree = doc.getElementById("igLinksTree");
 	
 	for (var i = 0; i < tree.view.rowCount; i++) {  
-		var shakespeare = doc.getElementById("tobeornottobe_" + i);
-		shakespeare.setAttribute("value", value);
-		var row = doc.getElementById("row_" + i);
-		row.setAttribute("properties", value == "true"?"checked":"");
+		// var shakespeare = doc.getElementById("tobeornottobe_" + i);
+		// shakespeare.setAttribute("value", value);
+		// var row = doc.getElementById("row_" + i);
+		// row.setAttribute("properties", value == "true"?"checked":"");
+		var CheckboxColIdx = tree.columns.getNamedColumn("tobeornottobe").index;
+		var row = tree.view.getItemAtIndex(i).firstChild;
+		var shakespeare = row.childNodes[CheckboxColIdx];
+		var newVal = (value == "toggle" ? !(shakespeare.getAttribute("value") == "true") : value == "true");
+		shakespeare.setAttribute("value", newVal);
+		row.setAttribute("properties", newVal ?"checked":"");
 	}
 	
 	updateCounter();
@@ -156,9 +199,10 @@ function applyCmd(cmd) {
 	var doc = this.document;
 	var tree = doc.getElementById("igLinksTree");
 	if (tree.view.rowCount == 0) return;
-	
-	var nWinCheck = false;
-		
+
+	var CheckboxColIdx = tree.columns.getNamedColumn("tobeornottobe").index;
+	var UrlColIdx = tree.columns.getNamedColumn("url").index;
+
 	var start = new Object();
 	var end = new Object();
 	var numRanges = tree.view.selection.getRangeCount();
@@ -166,30 +210,27 @@ function applyCmd(cmd) {
 	for (var i = 0; i < numRanges; i++) {
 		tree.view.selection.getRangeAt(i, start, end);
 		for (var j = start.value; j <= end.value; j++) {
+			var row = tree.view.getItemAtIndex(j).firstChild;
+			var shakespeare = row.childNodes[CheckboxColIdx];
 			switch(cmd) {
 				case "true":
 				case "false":
-					var shakespeare = doc.getElementById("tobeornottobe_" + j);
 					shakespeare.setAttribute("value", cmd);
-					var row = doc.getElementById("row_" + j);
 					row.setAttribute("properties", cmd == "true"?"checked":"");
 					break;
 				case "toggle":
-					var shakespeare = doc.getElementById("tobeornottobe_" + j);
 					shakespeare.setAttribute("value", !(shakespeare.getAttribute("value") == "true"));
-					var row = doc.getElementById("row_" + j);
 					row.setAttribute("properties", shakespeare.getAttribute("value") == "true"?"checked":"");
 					break;
 				case "open":
-					/*var reqUrl = doc.getElementById("url_" + j).getAttribute("value");
-					if (!nWinCheck) {
-						var nWin = window.open(reqUrl,reqUrl,"menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes");
-						nWinCheck = true;
+					/* var reqUrl = row.childNodes[UrlColIdx].getAttribute("value");
+					if (!nWin) {
+						var nWin = window.open(reqUrl,reqUrl,"menubar=yes,toolbar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes");
 					} 
 					else {
 						var win = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow("navigator:browser");
-						win.openUILinkIn(reqUrl, "tab");
-					}*/
+						win.gBrowser.addTab(reqUrl);
+					} */
 					break;
 				default:
 			}
@@ -228,7 +269,9 @@ function applyFilter() {
 	var currentId = 0;
 	
 	for (var i = firstPage; i <= lastPage; i++) {
-		for (var j = 0; j < objLinks.links[i].length; j++) {
+		for (var j = 0; j < objLinks.links[i].length; j++, currentId++) {
+			if (doc.getElementById("req_" + currentId).getAttribute("hidden") == "true") continue;
+			
 			var shakespeare = doc.getElementById("tobeornottobe_" + currentId);
 			var matched = false;
 			
@@ -239,8 +282,6 @@ function applyFilter() {
 			shakespeare.setAttribute("value", matched);
 			var row = doc.getElementById("row_" + currentId);
 			row.setAttribute("properties", matched?"checked":"");
-
-			currentId++;
 		}
 	}
 
@@ -315,10 +356,9 @@ function chgPreview(initialChange) {
 		var currentId = 0;
 
 		for (var a = firstPage; a <= lastPage; a++) {
-			for (var b = 0; b < objLinks.links[a].length; b++) {
+			for (var b = 0; b < objLinks.links[a].length; b++, currentId++) {
 				var treeCell = document.getElementById("url_" + currentId);
 				if (objLinks.thumbs[a][b]) treeCell.setAttribute("src", objLinks.thumbs[a][b].src);
-				currentId++;
 			}
 		}
 		pbox.hidden = true;
@@ -367,6 +407,11 @@ function changeImage() {
 	if (tree.view.rowCount == 0) return;
 	
 	var current = tree.view.selection.currentIndex;
+	if (current >= 0) {
+		var current_treeItem = tree.view.getItemAtIndex(current);
+		for (current = 0; current_treeItem = current_treeItem.previousSibling; current++);
+		}
+	
 	var thumb = doc.getElementById("thumbnail");
 	
 	var previewLabel = doc.getElementById("previewLabel");
@@ -417,26 +462,29 @@ function onTreeClicked(event) {
 	// Get the row, column and child element at the point
 	var row = {}, col = {}, child = {};
 	tbo.getCellAt(event.clientX, event.clientY, row, col, child);
+	if (col.value.type != Components.interfaces.nsITreeColumn.TYPE_CHECKBOX) return;
+	
+	// var shakespeare = doc.getElementById("tobeornottobe_" + row.value);
+	// var currentRow = doc.getElementById("row_" + row.value);
+	var currentRow = tree.view.getItemAtIndex(row.value).firstChild;
+	var value = tbo.view.getCellValue(row.value, col.value);
 
-	var shakespeare = doc.getElementById("tobeornottobe_" + row.value);
-	var currentRow = doc.getElementById("row_" + row.value);
-  
-	currentRow.setAttribute("properties", shakespeare.getAttribute("value")=="true"?"checked":"");
+	currentRow.setAttribute("properties", value=="true"?"checked":"");
 	
 	updateCounter();
 }
 
 
 function updateCounter() {
-   var doc = this.document;
-   var strbundle = doc.getElementById("imagegrabber-strings");
-   
-   var tree = doc.getElementById("igLinksTree");
-   var counter = 0;
-   
-   for (var i = 0; i < tree.view.rowCount; i++) {
-      if (doc.getElementById("row_" + i).getAttribute("properties") == "checked") counter++;
-   }
-   with (doc.getElementById("selectionCounter"))
-      value = strbundle.getFormattedString("images_selected_counter", [counter, tree.view.rowCount]);
+	var doc = this.document;
+	var strbundle = doc.getElementById("imagegrabber-strings");
+
+	var tree = doc.getElementById("igLinksTree");
+	var counter = 0;
+
+	for (var i = 0; i < tree.view.rowCount; i++) {
+		if (tree.view.getItemAtIndex(i).firstChild.getAttribute("properties") == "checked") counter++;
+		}
+	with (doc.getElementById("selectionCounter"))
+		value = strbundle.getFormattedString("images_selected_counter", [counter, tree.view.rowCount]);
 }
